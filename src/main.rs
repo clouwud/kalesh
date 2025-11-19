7a59d61979d1c739b06c6b99e2639e6c667ddbff
clouwud <dakshgajbhiye902@gmail.com>
2025-11-19 19:43:17 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::env;
+use std::io::{self, Write};
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+fn main() {
+    loop {
+        // showing current dir in prompt
+        let cwd = env::current_dir().unwrap_or_else(|_| "/".into());
+        // Get the username from the environment variable
+        let username = env::var("USER").unwrap_or_else(|_| String::from("unknown_user"));
+        // print!("[kalesh@{}]$ ", cwd.display());
+        print!("[{}@{}]$ ", username, cwd.display());
+        io::stdout().flush().unwrap();
+
+        // read input
+        let mut input = String::new();
+        if io::stdin().read_line(&mut input).is_err() {
+            println!();
+            break;
+        }
+
+        let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+
+        // exit build-in
+        if input == "exit" {
+            println!("kalesh: exiting...");
+            break;
+        }
+
+        // parse tokens
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
+        }
+
+        // handle build-in cd
+        if parts[0] == "cd" {
+            let target = parts.get(1).cloned().unwrap_or("/");
+            if let Err(err) = env::set_current_dir(Path::new(target)) {
+                eprintln!("kalesh: cd: {}: {}", target, err);
+            }
+            continue;
+        }
+
+        // external commands
+        let mut cmd = Command::new(parts[0]);
+        cmd.args(&parts[1..]);
+
+        // inherit stdin/stdout/stderr
+        cmd.stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit());
+
+        match cmd.spawn() {
+            Ok(mut child) => {
+                child.wait().expect("failed to wait on child");
+            }
+            Err(err) => {
+                eprintln!("kalesh: command not found: {}", err);
+            }
+        }
+    }
+}
